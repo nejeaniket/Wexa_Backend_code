3811ebe66cd9b7028b7ebb87089a077ad8f1234a
nejeaniket <nejeaniket@gmail.com>
2026-08-18 00:25:06 +0530

    Api

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: [clientOrigin, "http://localhost:5173", "http://localhost:5174"],
+    origin: [clientOrigin, "http://wexagraph.netlify.app"],
   }),
 );
 app.use(express.json());
